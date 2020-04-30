14ca8fdf3638bcc8ff4c76873ec5f5e26ddb318e
habaka26 <62945694+habaka26@users.noreply.github.com>
2020-04-30 17:22:30 +0600

    Add files via upload

---
+++ informatika/app.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        var $this = $(this),
+        blockId = $(this).data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500)
+    });
+});
