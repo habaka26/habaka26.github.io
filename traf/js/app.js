b90bafd6f0db8df48424ad2fc5439ec6eb40c78f
habaka26 <62945694+habaka26@users.noreply.github.com>
2020-05-09 13:57:55 +0600

    Add files via upload

---
+++ traf/js/app.js
@@ -0,0 +1,15 @@
+new WOW().init();
+
+$(function(){
+
+    $("[data-collapse]").on("click", function(event){
+
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $(this).data('collapse');	
+        $this.toggleClass("active");
+
+    });
+
+});
