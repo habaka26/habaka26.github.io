5ee2c3860c0d7ad9d6537027bbd0c874b7012c56
habaka26 <62945694+habaka26@users.noreply.github.com>
2020-07-04 18:14:28 +0600

    Add files via upload

--- adalazia/js/script.js
+++ adalazia/js/script.js
@@ -95,3 +95,21 @@ document.body.onload = function() {
         }
     }, 1000);
 }
+
+function backToTop() {
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >=50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    })
+
+    button.on('click', (e) => {
+        e.preventDefault()
+        $('html').animate({scrollTop: 0}, 1000)
+    })
+}
+backToTop();
